e46ecb45a203e9ea826921c4dc3073ca10877736
takeru1293 <68981576+takeru1293@users.noreply.github.com>
2021-10-31 18:43:49 +0900

    タスクバーのレイアウト修正

--- src/conpornent/TitleBar/TitleBar.jsx
+++ src/conpornent/TitleBar/TitleBar.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import { TitleBarBtn } from "./TitleBarBtn";
 
 export const TitleBar = () => {
+  const style = {
+    backgroundColor: "#ffbbff",
+    height: "75px",
+    width: "100%"
+  };
+
   return (
-    <div>
+    <div style={style}>
       <TitleBarBtn titleName="aaaaaaa" />
       <TitleBarBtn titleName="bbbbbbb" />
       <TitleBarBtn titleName="ccccccc" />
--- src/conpornent/TitleBar/TitleBarBtn.jsx
+++ src/conpornent/TitleBar/TitleBarBtn.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 
+const style = {
+  fontSize: "30px",
+  textDecoration: "none",
+  color: "#fff",
+  fontWeight: "bold",
+  width: "20%",
+  height: "100%",
+  textAlign: "center",
+  borderRadius: "0px",
+  float: "left",
+  margin: "0px",
+  border: "solid"
+};
+
 export const TitleBarBtn = (prop) => {
   const { titleName } = prop;
   return (
-    <div>
-      <button>{titleName}</button>
-    </div>
+    <button class="titlename" style={style}>
+      {titleName}
+    </button>
   );
 };
